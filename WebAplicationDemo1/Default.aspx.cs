50b05600e32b9803c6718e18b5eaf510107a2c47
ImageSolutions <chipanamarco@hotmail.com>
2014-07-12 12:00:35 -0400

    cambio

--- WebAplicationDemo1/Default.aspx.cs
+++ WebAplicationDemo1/Default.aspx.cs
@@ -12,6 +12,7 @@ namespace WebAplicationDemo1
         protected void Page_Load(object sender, EventArgs e)
         {
             var a = "test";
+            var test = "test1";
         }
     }
 }
